148511a4e9e92e84b32b02127b7559bb7379b88c
Sagar Satapathy <sagarsatapathy.satapathy1@gmail.com>
2023-06-14 17:48:43 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sagarbaba.github.io",
-  base: "/sagar-portfolio",
   server: {
     port: 4000,
   },
