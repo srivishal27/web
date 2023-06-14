760547d3e3d88313e0118c436ceb042714724bfe
Sagar <sagarsatapathy.satapathy1@gmail.com>
2023-06-14 17:22:04 +0530

    final_change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sambeetpanda507.github.io",
+  site: "https://sagarbaba.github.io",
   base: "/sagar-portfolio",
   server: {
     port: 4000,
